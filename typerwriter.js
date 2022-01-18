414514946db9b78f450ef8156bf25ff0f283873e
glitjch <t.jamesphan@gmail.com>
2022-01-18 18:14:32 +0000

    practicing animated typewriting with setTimeOut

---
+++ typerwriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+
+let i = 1000;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, i);
+  i = i + 60;
+}
+  
+setTimeout(() => {
+  process.stdout.write("\n");
+},
+i
+);
+
